5527f29a63d9f16ec99dcc2a1875d3d6450273af
Hector Garcia <hectoregarciap@hotmail.com>
2019-05-17 16:31:32 -0500

    organizacion en modulo.  este es una version BASE para otros proyectos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+
+import { SharedModule } from "../shared/shared.module";
+import { PAGES_ROUTES } from "./pages.routes";
+
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { Graficas1Component } from "./graficas1/graficas1.component";
+import { PagesComponent } from "./pages.component";
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  exports: [DashboardComponent, ProgressComponent, Graficas1Component],
+  imports: [SharedModule, PAGES_ROUTES]
+})
+export class PagesModule {}
+//exports: [DashboardComponent, ProgressComponent, Graficas1Component]
+//para que otros modulos lo puedan accesar, sino solo es visible solo dentro de este modulo
+// POR EJEMPLO PagesComponent NO lo podemos acceder desde afuera
